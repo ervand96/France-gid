c08640c312b132808d121c2440d6d998898a7ff4
miqo221 <miqonazarian@gmail.com>
2026-04-18 21:06:38 +0400

    translation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,13 +4,13 @@ import HeroSection from "@/app/shared/HeroSection";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("Home");
+  const t = useTranslations("HeroSection");
   return (
     <div>
       <HeroSection
-        heading={t("title")}
-        coloredPart="Like Never Before"
-        subHeading="Exclusive guided tours revealing the hidden soul of the City of Light. Authentic experiences crafted for discerning travelers."
+        heading={t("ExperienceParis")}
+        coloredPart={t("LikeNeverBefore")}
+        subHeading={t("ExclusiveGuided")}
       />
     </div>
   );
