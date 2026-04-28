e8a22d20fa91a2c4f925a055ee17182f85571ad5
ervand96 <ervandharutyunyan06096@gmail.com>
2026-04-28 16:01:14 +0400

    fixed

--- src/app/shared/AboutGuideSection/type.ts
+++ src/app/shared/AboutGuideSection/type.ts
@@ -1,6 +1,4 @@
 interface AboutData {
-  title: string;
-  subtitle: string;
   description: string;
   stats: Array<{ id: number; value: string; label: string }>;
   gallery: Array<{ id: number; url: string }>;
