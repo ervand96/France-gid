b6673aa3c209c6e5c5f1dd7a4a690a5877c79cd7
ervand96 <ervandharutyunyan06096@gmail.com>
2026-05-02 03:10:10 +0400

    fixed

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 const locales: readonly string[] = ["ru", "en"];
 const defaultLocale: string = "ru";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const cookieLocale = request.cookies.get("NEXT_LOCALE")?.value;
